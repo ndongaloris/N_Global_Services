fee31b251aadfabaf91361999a1aaa598e8ae5bc
ndongaloris <ndongajared@gmail.com>
2025-06-09 14:46:58 +0300

    contact first part

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,26 +1,43 @@
 function Contact(){
     return (
-        <form action="" id="contactUs">
-            <h2>Send Us a Message</h2>
-            <p>Send us a message and we will respond in 24h.</p>    
-            <div>
-                <label htmlFor="">First Name:</label>
-                <input type="text" />
-                <label htmlFor=""> Last Name:</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label htmlFor=""> Email:</label>
-                <input type="email" placeholder="myemail@gmail.com" />
-                
-                <label htmlFor=""> Phone Number:</label>
-                <input type="number" />
-            </div>
-            <label htmlFor="">
-                <textarea name="" placeholder="Type your message here" id=""></textarea>
-            </label>
-            <button>Send</button>
-        </form>
+        <div id="Contact">
+            <section>
+                <form action="" id="contactUs">
+                    <h2>Send Us a Message</h2>
+                    <p>Send us a message and we will respond in 24h.</p>    
+                    <div>
+                        <fieldset>
+                            <legend>Your Name</legend>
+                            <label htmlFor="">First Name:
+                                <input type="text" />
+                            </label>
+                            <label htmlFor=""> Last Name:
+                                <input type="text" />
+                            </label>
+                        </fieldset>
+                    </div>
+                    <div>
+                        <fieldset>
+                            <legend>Contact details</legend>
+                            <label htmlFor=""> Email:
+                                <input type="email" placeholder="myemail@gmail.com" />
+                            </label>
+                            
+                            <label htmlFor=""> Phone Number:
+                                <input type="number" />
+                            </label>
+                        </fieldset>
+                    </div>
+                    <textarea name="" placeholder="Type your message here" id=""></textarea>
+                    <button>Send</button>
+                </form>
+            </section>
+            <section id="contactMine">
+                <h2>Contact Information</h2>
+                <p>ndongajared@gmail.com</p>
+                <p>+375-25-697-8184</p>
+            </section>
+        </div>
     )
 }
 
